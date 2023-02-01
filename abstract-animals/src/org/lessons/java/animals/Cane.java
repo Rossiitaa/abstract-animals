c58a47945f86dcad8f971ad39ce45debc4a29365
Rossiitaa <rosscuotto1998@gmail.com>
2023-02-01 15:06:36 +0100

    Cane class added

--- abstract-animals/src/org/lessons/java/animals/Cane.java
+++ abstract-animals/src/org/lessons/java/animals/Cane.java
@@ -1,5 +1,19 @@
 package org.lessons.java.animals;
 
-public class Cane {
+public class Cane extends Animale {
+
+	public Cane(String nome) {
+		super(nome);
+	}
+
+	@Override
+	public void verso() {
+		System.out.println("bau bau");
+	}
+
+	@Override
+	public void mangia() {
+		System.out.println("è onnivoro");
+	}
 
 }
