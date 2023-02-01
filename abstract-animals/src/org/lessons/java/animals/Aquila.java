254b25d3b4eaba37dda1b55d500c6033e9b7ee9c
Rossiitaa <rosscuotto1998@gmail.com>
2023-02-01 15:09:08 +0100

    Aquila and Delfino classes added

--- abstract-animals/src/org/lessons/java/animals/Aquila.java
+++ abstract-animals/src/org/lessons/java/animals/Aquila.java
@@ -1,5 +1,19 @@
 package org.lessons.java.animals;
 
-public class Aquila {
+public class Aquila extends Animale {
+
+	public Aquila(String nome) {
+		super(nome);
+	}
+
+	@Override
+	public void verso() {
+		System.out.println("gridono o stridono");
+	}
+
+	@Override
+	public void mangia() {
+		System.out.println("vari tipi di animali");
+	}
 
 }
