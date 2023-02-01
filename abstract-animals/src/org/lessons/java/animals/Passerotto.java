b200d334b4bb70b21f68d4fdd096c6c6bd2ae188
Rossiitaa <rosscuotto1998@gmail.com>
2023-02-01 15:07:02 +0100

    Passerotto class added

--- abstract-animals/src/org/lessons/java/animals/Passerotto.java
+++ abstract-animals/src/org/lessons/java/animals/Passerotto.java
@@ -1,5 +1,19 @@
 package org.lessons.java.animals;
 
-public class Passerotto {
+public class Passerotto extends Animale {
+
+	public Passerotto(String nome) {
+		super(nome);
+	}
+
+	@Override
+	public void verso() {
+		System.out.println("cip cip");
+	}
+
+	@Override
+	public void mangia() {
+		System.out.println("frutta, semi, pastoncini all'uovo o sfarinati, uovo fresco");
+	}
 
 }
