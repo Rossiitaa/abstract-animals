c102ea5eb78eb8c451ecc8a10cdd75b46cf47034
Rossiitaa <rosscuotto1998@gmail.com>
2023-02-01 15:00:22 +0100

    Animale class added

--- abstract-animals/src/org/lessons/java/animals/Animale.java
+++ abstract-animals/src/org/lessons/java/animals/Animale.java
@@ -1,5 +1,25 @@
 package org.lessons.java.animals;
 
-public class Animale {
+public abstract class Animale {
+	private String nome;
 
+	public Animale(String nome) {
+		setNome(nome);
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void dormi() {
+		System.out.println("Zzz");
+	}
+
+	public abstract void verso();
+
+	public abstract void mangia();
 }
